7f464c0c2cd43f483021ea120c08a64068bfaa2b
David Temple <dstemple7@gmail.com>
2020-07-07 17:57:53 -0500

    mVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import axios from 'axios'
 
 class App extends React.Component {
   state ={
     username: '',
-    name: ''
+    name: '',
+    img: '',
+    location: '', 
+    followers:[]
   }
 
   componentDidMount(){
@@ -13,25 +15,45 @@ class App extends React.Component {
     .then(res => {
       this.setState({
         username: res.data.login,
-        name: res.data.name
+        name: res.data.name,
+        location: res.data.location,
+        img: res.data.avatar_url
       })
     })
   }
 
   handleChanges = e => {
     this.setState({
-      username: e.target.value
+      login: e.target.value
     })
   }
 
   fetchUsers = e => {
     e.preventDefault()
     axios
-      .get(`https://api.github.com/users/${this.state.username}`)
+      .get(`https://api.github.com/users/${this.state.login}`)
       .then(res => {
         this.setState ({
           username:res.data.login,
-          name: res.data.name
+          name: res.data.name,
+          location: res.data.location,
+          img: res.data.avatar_url
+        })
+      })
+  }
+
+  fetchFollowers = e => {
+    e.preventDefault()
+    axios
+      .get(`https://api.github.com/users/${this.state.login}/followers`)
+      .then(res => {
+        this.setState ({
+          followers: res.data.map(follower => (
+            <div>
+              <img src={follower.avatar_url}/>
+              <h5>Git @ me @ {follower.url}</h5>
+            </div>
+          ))
         })
       })
   }
@@ -39,16 +61,22 @@ class App extends React.Component {
   render(){
   return (
     <div>
-      <h1>GitHub User</h1>
+      <h1>Are we using GitHub or is GitHub aka Microsoft using us???</h1>
       <input
         type='text'
-        value={this.state.username}
+        value={this.state.login}
         onChange={this.handleChanges}
       />
+      <br></br>
       <button onClick={this.fetchUsers}>Fetch User</button>
+      <button onClick={this.fetchFollowers}>Fetch Followers</button>
       <div>
-        <h3>{this.state.username},  {this.state.name}</h3>
-      </div>     
+        <h3>{this.state.username} aka {this.state.name} from {this.state.location}</h3>
+        <img src={this.state.img}/>
+        <br></br>
+        <h1>My Followers</h1>
+        {this.state.followers}
+      </div>
     </div>
   )}
 }
